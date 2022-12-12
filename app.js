cfdf3d7f2d9aa19f691988eefb3182e7ecf54552
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2022-12-12 10:38:07 +0100

    Loading API data indicator

--- app.js
+++ app.js
@@ -4,24 +4,31 @@ const pobierzKursy = async () => {
   return kursy[0];
 };
 
-const wypelnijTabele = (tabelaKursow) => {
+const wypelnijTabele = (tabelaKursow, isLoading = false) => {
   const loc = document.querySelector('tbody');
   loc.innerHTML = '';
+  if (isLoading) {
+    loc.innerHTML += `<tr><td colspan='2'>Trwa ładowanie</td></tr>`;
+    return;
+  }
   tabelaKursow.forEach((kurs) => {
     loc.innerHTML += `<tr>
         <td>${kurs.currency}</td>
         <td>${kurs.mid}</td>
       </tr>`;
   });
-}
+};
 
 const wypelnijDateKursu = (date) => {
   const loc = document.querySelector('span');
   loc.innerText = `Tabela kursów z dnia ${date}`;
-}
+};
 
 document.querySelector('#pobierz')
-  .addEventListener('click', () => pobierzKursy().then(dane => {
-    wypelnijDateKursu(dane.effectiveDate);
-    wypelnijTabele(dane.rates);
-  }));
+  .addEventListener('click', () => {
+    wypelnijTabele([], true);
+    pobierzKursy().then(dane => {
+      wypelnijDateKursu(dane.effectiveDate);
+      wypelnijTabele(dane.rates);
+    })
+  });
